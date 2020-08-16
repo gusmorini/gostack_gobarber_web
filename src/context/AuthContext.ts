a1e444ccb17989536a0cebd6e086e681e98d9d56
Gustavo Morini <gustavommh@gmail.com>
2020-08-16 08:43:31 -0300

    api, auth context

--- src/context/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface AuthContext {
-  name: string;
-}
-
-const AuthContextData = createContext<AuthContext>({} as AuthContext);
-
-export default AuthContextData;
